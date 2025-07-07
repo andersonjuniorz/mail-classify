64b02ab20d99af496d0a35c7ff13c3f95ba86bf0
andersonjuniorz <dev.andersonjuniorz@gmail.com>
2025-07-07 02:48:21 -0300

    Integrate Frontend and Backend in Docker Compose Orchestration

--- Backend/myApp/__init__.py
+++ Backend/myApp/__init__.py
@@ -4,10 +4,17 @@
 
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
 
+import os
+
+load_dotenv()
+
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "http://localhost") 
+                       
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+    CORS(app, resources={r"/*": {"origins": [FRONTEND_ORIGIN]}})
 
     # Importa e registra as rotas
     from .routes import upload_files
